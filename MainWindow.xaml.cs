d7ac1b2ede9ae9669cae1c5f36fd23030d7a2a7e
Kudoes <kashifmkhan95@hotmail.com>
2017-01-28 09:50:13 -0700

    Displays the current date and time in a textbox on the screen
    Implemented a rudimentary sort of "check" that throws an exception if
    the date-time on the system matches the date-time in the String.
    
    Can expand on this to check the system time with a list of "time"
    strings in an alarm ArrayList or something similar.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace SENG403
 {
@@ -23,6 +24,28 @@ namespace SENG403
         public MainWindow()
         {
             InitializeComponent();
+
+            startclock();
+        }
+
+        private void startclock()
+        {
+            DispatcherTimer timer = new DispatcherTimer();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += tickevent;
+            timer.Start();
+        }
+
+        private void tickevent(object sender, EventArgs e)
+        {
+            timeLabel.Text = DateTime.Now.ToString();
+            Exception exception = new Exception();
+            if (timeLabel.Text == "2017-01-28 1:47:00 AM")
+            {
+                throw exception;    // This works
+            }
+
         }
     }
 }
