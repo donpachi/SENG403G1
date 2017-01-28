ea1a4ae1d9a363c98624e319090e59c911757b3c
Kudoes <kashifmkhan95@hotmail.com>
2017-01-28 12:28:05 -0700

    Split the date/time into two strings
    Split the date/time into two strings for better control and checking
    abilities, and included more comments in the program

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,23 +25,39 @@ namespace SENG403
         {
             InitializeComponent();
 
+            // Start the clock
             startclock();
         }
 
+        // The method that creates a tick event and sets the interval
         private void startclock()
         {
+            // Create a timer object
             DispatcherTimer timer = new DispatcherTimer();
-
-            timer.Interval = TimeSpan.FromSeconds(1);
-            timer.Tick += tickevent;
-            timer.Start();
+            timer.Interval = TimeSpan.FromSeconds(1);   // Set the timer interval to one second
+            timer.Tick += tickevent;    // After every one second, we trigger a tick event
+            timer.Start();  // Start the timer
         }
 
+        // Tick event that occurs every one second
         private void tickevent(object sender, EventArgs e)
         {
-            timeLabel.Text = DateTime.Now.ToString();
+            // Get the current date and time and convert it to a single string
+            // In the form: 'yyyy-mm-dd hh:mm:ss AM/PM' (i.e: 2017-01-28 12:20:00 PM)
+            String dateAndTime = DateTime.Now.ToString();
+            
+            // An exception for testing purposes
             Exception exception = new Exception();
-            if (timeLabel.Text == "2017-01-28 1:47:00 AM")
+
+            // Split the dateAndTime string into an array of two entries, one for date and one for time
+            String[] dateAndTimeArray = dateAndTime.Split(new char[] { ' ' }, 2); // Split at the first occurence of ' ' only
+            String date = dateAndTimeArray[0];  // Store the date in the form: yyyy-mm-dd
+            String time = dateAndTimeArray[1];  // Store the time in the form: hh:mm:ss AM/PM
+            timeLabel.Text = time;  // Display only the time on the UI
+
+            // Check every second if the current time is the one we're checking for
+            // If so, throw an exception
+            if (time == "12:20:00 PM")
             {
                 throw exception;    // This works
             }
