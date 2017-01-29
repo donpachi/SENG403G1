80007be91286aa2920d80f1eae863f08a0fdf7b6
Austin Sullivan <Austin Sullivan>
2017-01-29 15:58:48 -0700

    successfully playing .wav file once

--- soundModule.cs
+++ soundModule.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using SENG403.Properties;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +11,23 @@ namespace SENG403
 {
     class SoundModule
     {
-        String paths = Path.Combine(Directory.GetCurrentDirectory(), "\\Sounds");
-        
+
         public void playSound()
         {
-            System.Media.SoundPlayer player = new System.Media.SoundPlayer();
-            //player.SoundLocation = path;
-            System.Diagnostics.Debug.WriteLine("directory:   "+paths);
+            Stream theSound = Resources.squarearp1;
+
+            try
+            {
+                SoundPlayer player = new SoundPlayer(theSound);
+                player.Play();
+            }
+            catch(FileNotFoundException)
+            {
+                System.Diagnostics.Debug.WriteLine("Error: sound file not found");
+            }
+            
+           
+
         }
     }
 }
