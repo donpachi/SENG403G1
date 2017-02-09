78133b9192323f0a704bd39dfadbe0a7b2ba208f
Austin Sullivan <austinmclastro@gmail.com>
2017-02-08 19:41:17 -0700

    All required UI in place, all event listeners for UI in MainWindow. UI takes up only small part of screen, but is visible and works (considered a bug).

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,32 +22,173 @@ namespace SENG403
     public partial class MainWindow : Window
     {
         SoundModule sound = new SoundModule();
-        
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        // TEMPORARY - Austin
-        /*
-        // This button event simulates what would be done in the UI to select a sound the user wants
-        // for the particular alarm the user is setting up.
-        private void soundbutton_Click(object sender, RoutedEventArgs e)
-        {
-            if (sound.isPlaying())
-                sound.stopSound();
-            
-            else{
-                try{
-                    sound.playSound(sound.getSound(0));
-                }
-                catch(Exception)
-                {
-                    System.Diagnostics.Debug.WriteLine("Error: no .wav files in Sounds folder");
-                }
-            }
-        }*/
 
+        //=================================================================== alarm set screen listeners
+        // Click the confirm button on the set alarm screen
+        // (clicking this should add the alarm to the alarm ArrayList)
+        private void clickButtonConfirm(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Click the cancel button on the set alarm screen
+        // (clicking this should go back to the time display screen)
+        private void clickButtonCancel(object sender, RoutedEventArgs e)
+        {
+            canvasAlarmSet.Visibility = System.Windows.Visibility.Hidden;
+            canvasTimeDisplayBase.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        // Listener for when an alarm sound from dropdown menu is selected (comboBox)
+        // I am not 100% sure on this type of event listener though
+        private void selectedSound(object sender, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+
+        //-------------------------------------------------------------checkboxes
+        // Checked the repeat weekly checkbox
+        private void repeatWeeklyIsChecked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Checked the Sunday checkbox for an alarm
+        private void checkedSunday(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Checked the Monday checkbox for an alarm
+        private void checkedMonday(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Checked the Tuesday checkbox for an alarm
+        private void checkedTuesday(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Checked the Wednesday checkbox for an alarm
+        private void checkedWednesday(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Checked the Thursday checkbox for an alarm
+        private void checkedThursday(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Checked the Friday checkbox for an alarm
+        private void checkedFriday(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Checked the Saturday checkbox for an alarm
+        private void checkedSaturday(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //-------------------------------------------------------------endcheckboxes
+
+        //-------------------------------------------------------------radiobuttons
+        // Selected the 1 minute snooze radio button for an alarm
+        private void select1Min(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Selected the 2 minute snooze radio button for an alarm
+        private void select2Mins(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Selected the 5 minute snooze radio button for an alarm
+        private void select5Mins(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Selected the 10 minute snooze radio button for an alarm
+        private void select10Mins(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Selected the 15 minute snooze radio button for an alarm
+        private void select15Mins(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Selected the 30 minute snooze radio button for an alarm
+        private void select30Mins(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Selected the 60 minute snooze radio button for an alarm
+        private void select60Mins(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //-------------------------------------------------------------end radiobuttons
+        //=================================================================== end alarm set screen
+
+
+
+        //=================================================================== active alarm screen listeners
+        // Listener for when the snooze button is pressed
+        private void clickSnooze(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        // Listener for the Dismiss button
+        private void clickDismiss(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //=================================================================== end active alarm screen listeners
+
+
+
+        //=================================================================== time display screen listeners
+        // Listener for when the toggleButton is checked
+        // when clicked, if state is analog, switch to digital. Else switch to analog.
+        private void displayModeToggle(object sender, RoutedEventArgs e)
+        {
+            if (canvasTimeDisplayAnalog.IsVisible)
+            {
+                canvasTimeDisplayAnalog.Visibility = System.Windows.Visibility.Hidden;
+                canvasTimeDisplayDigital.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                canvasTimeDisplayAnalog.Visibility = System.Windows.Visibility.Visible;
+                canvasTimeDisplayDigital.Visibility = System.Windows.Visibility.Hidden;
+            } 
+        }
+
+        // Listener for the set alarm button
+        // Hides the Time Display base canvas and makes the set alarm canvas visible
+        private void gotoSetAlarm(object sender, RoutedEventArgs e)
+        {
+            canvasTimeDisplayBase.Visibility = System.Windows.Visibility.Hidden;
+            canvasAlarmSet.Visibility = System.Windows.Visibility.Visible;
+        }
+        //=================================================================== end time display screen listeners
     }
 }
