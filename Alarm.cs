afba691cd43f1ffc801d53a03a82f86e002457a6
Kudoes <kashifmkhan95@hotmail.com>
2017-02-11 13:51:46 -0700

    Added comments in Alarm.cs
    - Used the '///' comment format to make some comments in Alarm.cs

--- Alarm.cs
+++ Alarm.cs
@@ -7,19 +7,22 @@ using System.Windows.Threading;
 
 namespace SENG403
 {
-
+    /// <summary>
+    /// Alarmhandler that handles setting up, creating and ending alarms.
+    /// It also handles starting a system clock and determining the current day of the week.
+    /// </summary>
     public class AlarmHandler
     {
-
         // Create a list of Alarms
         List<Alarm> alarmList;
+
+        // The current alarm; once an alarm has been triggered it is the current alarm
+        // Until the user dismisses it or snoozes it
         Alarm currentAlarm;
 
-
-
+        // Constructor for AlarmHandler class
         public AlarmHandler()
         {
-
             // Create a list of Alarms
             this.alarmList = new List<Alarm>();
 
@@ -30,29 +33,45 @@ namespace SENG403
             startclock();
         }
 
+        /// <summary>
+        /// Returns the currently ringing/active alarm
+        /// </summary>
+        /// <returns>The currently ringing alarm.</returns>
         public Alarm getCurrentAlarm()
         {
             return currentAlarm;
         }
 
-
-        //this ends the alarm and sets it to the next scheduled date if repeat is true, else it deletes the alarm
+        /// <summary>
+        /// End the alarm and set it to the next scheduled date if repeat is true
+        //  Otherwise, delete the alarm from the alarm list
+        /// </summary>
+        /// <param name="alarm">The alarm object to end or set to repeat</param>
         public void endAlarm(Alarm alarm)
         {
+            // If the repeat value is set to TRUE for the alarm, reset it
+            // So that it repeats at the same time the next week
             if (alarm.getRepeatVal() == true)
             {
                 alarm.reset();
             }
+
+            // Otherwise, delete the alarm from the alarm list
             else { deleteAlarm(alarm); }
         }
 
-        //this removes the specified alarm from alarmList even if it is set to repeat
+        /// <summary>
+        /// Remove the specified alarm from alarmList even if it is set to repeat
+        /// </summary>
+        /// <param name="alarm">Alarm object to delete from the alarm list.</param>
         public void deleteAlarm(Alarm alarm)
         {
             alarmList.Remove(alarm);
         }
 
-        // Start the timer, create a tick event and set tick interval to one second
+        /// <summary>
+        /// Start the timer, create a tick event and set tick interval to one second
+        /// </summary>
         private void startclock()
         {
             // Create a timer object
@@ -62,28 +81,33 @@ namespace SENG403
             timer.Start();  // Start the timer
         }
 
-        // Tick event that occurs every one second
+        /// <summary>
+        /// Tick event that occurs every one second
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void tickevent(object sender, EventArgs e)
         {
             // Get the CURRENT date and time in the form: 'yyyy-mm-dd hh:mm:ss AM/PM' (i.e: 2017-01-28 12:20:00 PM)
             DateTime dateAndTime = DateTime.Now;
 
-            // An exception for testing purposes
-            Exception exception = new Exception();
+            // Creates the day object
             int day = dayofweek(dateAndTime.Day, dateAndTime.Month, dateAndTime.Year);
+
             // Get only the time from the DateTime object
             String time = dateAndTime.ToLongTimeString();
 
             // Check every second if the current time is the one we're checking for
-            // If so, throw an exception (play sound later on)
+            // If so, set the alarm to ring
             foreach (Alarm alarm in alarmList)
             {
                 
-                // If the current time is one of the alarms, then throw an exception
+                // If the current time is one of the alarms, then check if the day is also correct
                 if (DateTime.Now.ToString("T").Equals(alarm.getDateTime().ToString()))
                 {
                     if (alarm.getDays() == "0000000" || alarm.getDays()[day].Equals('1'))
                     {
+                        // Play the alarm and set the current alarm to this alarm
                         currentAlarm = alarm;
                         currentAlarm.setRinging(true);
                     }
@@ -91,27 +115,31 @@ namespace SENG403
             }
         }
 
-        // Handle the input data once the "set alarm" button is pressed
-        //days string should be 7 digits long, "1" represents a selected day, "0" represents a non-selected day
+        /// <summary>
+        /// Handle the input data once the "set alarm" button is pressed
+        //  Days string should be 7 digits long, "1" represents a selected day, "0" represents a non-selected day
+        /// </summary>
+        /// <param name="time">The time the alarm is set to trigger on</param>
+        /// <param name="days">The days the alarm is set to trigger on</param>
+        /// <param name="alarmSound">The alarm sound set to play once the alarm goes off.</param>
         public void setNewAlarm(DateTime time, String days, SoundModule alarmSound)
         {
-            // Gather the input information and create a string representation of a DateTime object
-
-            // Add AM/PM to the time String
-            
-
             // Create a new alarm and append it to the alarmList
-            alarmList.Add(new Alarm(time, days, alarmSound)); //repeat value hard coded to false for now
-            //sort(alarmList);
+            alarmList.Add(new Alarm(time, days, alarmSound)); 
 
-            // Error checking the format of the input time still has to be handled
         }
 
-        //taken from stackoverflow, needs to be converted to C++
+        /// <summary>
+        /// Determines the day of the week and outputs a numeric representation of it
+        /// </summary>
+        /// <param name="d">Day</param>
+        /// <param name="m">Month</param>
+        /// <param name="y">Year</param>
+        /// <returns></returns>
         private int dayofweek(int d, int m, int y)
         {
-            ///the following can be replaced with (might be faster as well):
-            ///
+
+            // Determine the day of the week that the alarm is set to
             DateTime date1 = new DateTime(y, m, d);
             String date = date1.ToString("F");
             String day = date.Split(',')[0];
@@ -132,21 +160,15 @@ namespace SENG403
                 case "Saturday":
                     return 6;
             }
+
+            // Code should never reach here
             throw new Exception("Day of the week..");
-            //int[] t= { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
-            //if (m < 3)
-            //{
-            //    y -= m;
-            //}
-            //return (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7;
         }
-
-
-
-
     }
 
-    // The alarm class which holds the required data
+    /// <summary>
+    /// Alarm object containing all relevant information as to when the alarm goes off and what alarm sound it plays.
+    /// </summary>
     public class Alarm
     {
         String period; // AM or PM setting - may not be necessary
@@ -160,12 +182,16 @@ namespace SENG403
         public delegate void alarmEvent();
         public static event alarmEvent onRing;
 
+        /// <summary>
+        /// Return whether the alarm is ringing
+        /// </summary>
         public void AlarmRinging()
         {
             if (onRing != null)
                 onRing();
         }
 
+        // Alarm constructor
         public Alarm(DateTime time, string days, SoundModule alarmSound)
         {
             this.time = time;
@@ -176,44 +202,63 @@ namespace SENG403
             if (days != "0000000") { repeat = true; }
         }
 
-
-        // Return the Date and Time this alarm is set to
+        /// <summary>
+        /// Return the time this alarm is set to ring.
+        /// </summary>
+        /// <returns>String representation of the time the alarm is set to ring.</returns>
         public String getDateTime()
         {
             return time.ToLongTimeString();
         }
 
+        /// <summary>
+        /// Return the days the alarm is set to ring on.
+        /// </summary>
+        /// <returns>Days the alarm is ringing on.</returns>
         public String getDays() { return days; }
 
-        //This snoozes the alarm for a set amount of minutes
+        /// <summary>
+        /// Snoozes (delays) the alarm by the amount of minutes input
+        /// </summary>
+        /// <param name="minutes">Minutes that the user wants to snooze the alarm for.</param>
         public void snooze(Double minutes)
         {
             time = DateTime.Now.AddMinutes(minutes);
             alarmSound.stopSound();
         }
 
-        //this resets the alarm to the original pre-snooze configuration
+        /// <summary>
+        /// Reset the alarm to the original pre-snooze configuration.
+        /// </summary>
         public void reset() { time = settime; }
 
+        /// <summary>
+        /// Return whether the alarm is set to repeat or not
+        /// </summary>
+        /// <returns>The repeat value of the alarm.</returns>
         public Boolean getRepeatVal() { return repeat; }
 
 
         // Set the boolean value true/false depending on whether the alarm is ringing
+        /// <summary>
+        /// Set the boolean value true/false depending on whether the alarm is ringing
+        /// </summary>
+        /// <param name="val">Whether the alarm is ringing or not (true/false).</param>
         public void setRinging(bool val)
         {
             this.currentlyRinging = val;
             if (val == true)
             {
-                alarmSound.playSound();         //play alarm sound
+                alarmSound.playSound();
                 AlarmRinging();
             }
             else { alarmSound.stopSound(); }
         }
 
-
-
-
-        // Getter method to see if the current alarm is ringing
+        /// <summary>
+        /// Return whether the alarm is currently ringing or not.
+        /// </summary>
+        /// <returns>Boolean value representing the alarms ringing state.</returns>
         public bool isRinging()
         {
             return this.currentlyRinging;
